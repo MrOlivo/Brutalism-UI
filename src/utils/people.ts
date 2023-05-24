6baf47502dd383e4d49a0d5fdefe04cd628f0b1e
Edwin Olivo <46498127+MrOlivo@users.noreply.github.com>
2023-05-23 17:48:34 -0700

    Create people.ts

---
+++ src/utils/people.ts
@@ -0,0 +1,22 @@
+const artists = [
+    {
+      title: "Maria Miramar",
+      subtitle: "Berlin",
+      description: "Digital Creator",
+      img: "https://images.unsplash.com/photo-1535207010348-71e47296838a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
+    },
+    {
+      title: "Ben Karl",
+      subtitle: "Amsterdam",
+      description: "Audio-Video Artist",
+      img: "https://images.unsplash.com/photo-1507081323647-4d250478b919?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
+    },
+    {
+      title: "Alana David",
+      subtitle: "Berlin",
+      description: "Installation Artist",
+      img: "https://images.unsplash.com/photo-1517779548443-852211706367?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
+    },
+  ];
+
+  export {artists};
