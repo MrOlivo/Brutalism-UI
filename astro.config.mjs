733cb8605bee03b865cb4117f59c4a0222644929
Edwin Olivo <46498127+MrOlivo@users.noreply.github.com>
2024-03-08 10:26:29 -0700

    Update Astro configuration and package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://mrolivo.github.io",
-  base: "/Brutalism-UI",
+  site: 'https://mrolivo.github.io',
+  base: '/Brutalism-UI',
 });
