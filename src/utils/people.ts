bc8889f439dc645e1ab2829317a1122e74539d5e
Edwin Olivo <46498127+MrOlivo@users.noreply.github.com>
2024-03-08 10:58:39 -0700

    Update artists list in people.ts

--- src/utils/people.ts
+++ src/utils/people.ts
@@ -1,22 +1,40 @@
 const artists = [
-    {
-      title: "Maria Miramar",
-      subtitle: "Berlin",
-      description: "Digital Creator",
-      img: "https://images.unsplash.com/photo-1535207010348-71e47296838a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
-    },
-    {
-      title: "Ben Karl",
-      subtitle: "Amsterdam",
-      description: "Audio-Video Artist",
-      img: "https://images.unsplash.com/photo-1507081323647-4d250478b919?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
-    },
-    {
-      title: "Alana David",
-      subtitle: "Berlin",
-      description: "Installation Artist",
-      img: "https://images.unsplash.com/photo-1517779548443-852211706367?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
-    },
-  ];
+  {
+    title: "Maria Miramar",
+    subtitle: "Berlin",
+    description: "Digital Creator",
+    img: "https://images.unsplash.com/photo-1535207010348-71e47296838a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
+  },
+  {
+    title: "Ben Karl",
+    subtitle: "Amsterdam",
+    description: "Audio-Video Artist",
+    img: "https://images.unsplash.com/photo-1507081323647-4d250478b919?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
+  },
+  {
+    title: "Alana David",
+    subtitle: "Berlin",
+    description: "Installation Artist",
+    img: "https://images.unsplash.com/photo-1517779548443-852211706367?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
+  },
+  {
+    title: "Florence Pearson",
+    subtitle: "Dublin",
+    description: "Installation Artist",
+    img: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=1364&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Jonathan Wagner",
+    subtitle: "Dowakil",
+    description: "Circus Worker",
+    img: "https://images.unsplash.com/photo-1522556189639-b150ed9c4330?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTd8fHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D",
+  },
+  {
+    title: "Hana Kitagawa",
+    subtitle: "Yokohama",
+    description: "Anthropologist",
+    img: "https://plus.unsplash.com/premium_photo-1681492905478-07e94aa01581?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
 
-  export {artists};
+export { artists };
